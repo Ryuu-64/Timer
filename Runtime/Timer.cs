0d514eb580ee4199c331f7fee13b3987d341eac2
Ryuu-64 <2357622935@qq.com>
2022-05-02 01:21:05 +0800

    init

---
+++ Runtime/Timer.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+// ReSharper disable ParameterHidesMember
+
+namespace Ryuu
+{
+    [Serializable]
+    public class Timer : IDisposable
+    {
+        private MonoUpdater updater;
+        [SerializeField] private float timeStamp;
+        [SerializeField] private float timeSet;
+        [SerializeField] private float time;
+        [SerializeField] private bool isStop;
+        public event Action OnStop;
+
+        public bool IsStop => isStop;
+
+        public Timer(UpdateMode updateMode, Action onStop = null)
+        {
+            OnStop = onStop;
+            updater = Object.FindObjectOfType<MonoUpdater>();
+            updater.Subscribe(OnUpdate, updateMode);
+        }
+
+        public Timer SetUpdateMode(UpdateMode updateMode)
+        {
+            updater.Subscribe(OnUpdate, updateMode);
+            return this;
+        }
+
+        public Timer SetTime(float timeSet)
+        {
+            this.timeSet = timeSet;
+            return this;
+        }
+
+        public Timer SetCountDown()
+        {
+            timeStamp = Time.time;
+            time = timeSet;
+            return this;
+        }
+
+        public Timer Start()
+        {
+            isStop = false;
+            return this;
+        }
+
+        public void OnUpdate()
+        {
+            if (isStop)
+            {
+                return;
+            }
+
+            time = Time.time - timeStamp;
+
+            if (time < timeSet)
+            {
+                return;
+            }
+
+            isStop = true;
+            OnStop?.Invoke();
+        }
+
+        public Timer Stop()
+        {
+            isStop = true;
+            return this;
+        }
+
+        public static Timer Event(Action onStop, UpdateMode updateMode, float time, int repeat = 1)
+        {
+            Timer timer = new Timer(updateMode, onStop).SetTime(time).SetCountDown().Start();
+            if (repeat == -1)
+            {
+                timer.OnStop += () => { timer.SetCountDown().Start(); };
+            }
+            else
+            {
+                timer.OnStop += () =>
+                {
+                    repeat--;
+                    if (repeat == 0)
+                    {
+                        timer.Dispose();
+                    }
+                    else
+                    {
+                        timer.SetCountDown().Start();
+                    }
+                };
+            }
+
+            return timer;
+        }
+
+        public void Dispose()
+        {
+            updater.Unsubscribe(OnUpdate);
+        }
+    }
+}
